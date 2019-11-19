7830591377cec384c7ab60b0743b8886f7682103
bergzacstein <niccau@gmail.com>
2019-11-19 20:16:47 +0100

    window shape bug correction

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import numpy as np
 import skimage
 from utils import *
 import matplotlib.pyplot as plt
+import skimage.morphology as morpho
 
 #Sets current working directory
 #import os
@@ -47,25 +48,30 @@ def match_selection(image, selection, step = 4):
     """image : a numpy array corresponding to a picture.
     selection : ((topleft_x, topleft_y), (bottomright_x, bottoomright_y))"""
     #selection_im is the part of the image corresponding to the coordinate selection. 
-    selection_im = image[selection[0][0]:selection[1][0], selection[0][1]:selection[1][1]]
+    selection_im = image[selection[0][1]:selection[1][1], selection[0][0]:selection[1][0]]
     selection_width = np.abs(selection[0][0] - selection[1][0])
     selection_height = np.abs(selection[1][1] - selection[0][1])
-
     step = step #instead of doing pixel_wise comparison, we use a step. 
-    lines = image.shape[0] - selection_height
-    cols = image.shape[1] - selection_width
+    lines = image.shape[0] - selection_height 
+    cols = image.shape[1] - selection_width 
     #similarity_matrix holds the result of our similarity test between selection and
     # the subsection of image tested. 
     similarity_matrix = np.zeros((int(lines/step), int(cols/step)))
     for j in range(0, cols, step): #from left to right
         for i in range(0, lines, step): #from top to bottom
             # subsection is now a sample from our image. 
-            subsection = image[i:i+selection_width, j:j+selection_height]
+            subsection = image[i:i+selection_height, j:j+selection_width]
+            sub_selection_im = selection_im#[0:subsection.shape[0], 0:subsection.shape[1]]
             # We score the similarity between subsection and selection_im.
             # This can be done using various measures.
-            if selection_im.shape == subsection.shape: 
-                similarity = 1 - np.linalg.norm(selection_im - subsection) / np.linalg.norm(selection_im)
+            similarity = 1 - np.linalg.norm(sub_selection_im - subsection) / np.linalg.norm(selection_im)
+            try:
+                # instead of creating a smaller image
+                # add a square of color similarity and of shape selection_im to
+                # a big picture
                 similarity_matrix[int(i/step), int(j/step)] = similarity
+            except:
+                print(i/step,j/step)
     
     nb_floors = None
     return nb_floors, similarity_matrix
@@ -73,5 +79,12 @@ def match_selection(image, selection, step = 4):
 image = skio.imread('img/telecom.jpeg')
 im_grayscale = skimage.color.rgb2hsv(image)[:,:,2]
 selection = ((708, 393), (757, 529))
+#selection = ((1412, 2572), (1722, 2784))
+#selection = ((1380, 1380), (1550,1550))
+#selection = ((1835, 758), (1897, 949))
+nb_floors, similarity_matrix = match_selection(im_grayscale, selection, 8)
+lazy_imshow(similarity_matrix)
 
-nb_floors, similarity_matrix = match_selection(im_grayscale, selection)
+line_sum = np.sum(similarity_matrix, axis=1)
+plt.plot(line_sum)
+plt.show()
