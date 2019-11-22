17bffef267dc974a7d3ebdb895ed56715399b354
bergzacstein <niccau@gmail.com>
2019-11-22 14:54:14 +0100

    hyper parameters tuning

--- main_nico.py
+++ main_nico.py
@@ -82,8 +82,8 @@ def clean_projection(line, beta, wordy=False):
     """Juan's method of cleaning projected edges,
     using manual heuristics and moving average."""
     #line has a lot of noise on the leftmost and rightmost values. Remove them. 
-    line[:40] = 0
-    line[-40:] = 0
+    line[:10] = 0
+    line[-10:] = 0
     #Line also has negative values. TRASH them.
     line = line[line >= 0]
     #This graph is still very spiky. To smoothen it, we compute a moving average. 
@@ -249,7 +249,8 @@ def plot_boxes(boxes, scores, ax):
     for i, box in enumerate(boxes):
         width = np.abs(box[0][0] - box[1][0])
         height = np.abs(box[0][1] - box[1][1])
-        if height > np.quantile(h, 0.5):
+        #Lower the quantile to show more or less windows. 
+        if height > np.quantile(h, 0.2):
             bottomleft = (box[0][0], box[1][1])
             col = ((1-scores[i]/max(scores)), scores[i]/max(scores), 0)
             alpha = 0.5 + 0.5*scores[i]/max(scores)
@@ -276,7 +277,7 @@ def nicolas(imagefile):
     image = skio.imread(imagefile)
     gray_im = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image_eq = cv2.equalizeHist(gray_im)
-    floors, windows = juan_routine(image_eq, 0.95, 3, True, True, True)
+    floors, windows = juan_routine(image_eq, 0.9, 1, True, True, True)
 
     print('Finding and scoring boxes...')
     boxes, scores = get_boxes_and_scores(image, floors, windows, 0.8)
@@ -288,7 +289,7 @@ def nicolas(imagefile):
 
     nb_floors = guess_nb_floors(boxes, scores, 0.1, 0.99)
     print('Number of floors guessed : ', nb_floors)
-    return nb_floors, boxes, scores
+    return nb_floors
 
 nicolas('img/cropped_telecom.jpg')
 
